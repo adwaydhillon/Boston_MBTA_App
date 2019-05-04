40ad06e4fd409d7129ff3d0d72b840ecbd17cd95
Adway Dhillon <adhillon6@gatech.edu>
2019-05-04 18:06:38 -0400

    pending asynchronous streamlining

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -10,14 +10,20 @@ export class AppComponent {
   title: string;
   routeFilters: Map<any, any>;
   stopFilters: Map<any, any>;
-  subwayRoutes: any;
+  subwayRoutes: Map<string, string>;
+  routeToStopsDict: Map<any, any>;
+  maxStopRoute: any;
+  minStopRoute: any;
   
 
   constructor(public appService: AppService) {
     this.title = 'client';
     this.routeFilters = new Map();
     this.stopFilters = new Map();
-    this.subwayRoutes = [];
+    this.routeToStopsDict = new Map();
+    this.subwayRoutes = new Map();
+    this.maxStopRoute = {};
+    this.minStopRoute = {};
   }
 
   ngOnInit() {
@@ -25,49 +31,107 @@ export class AppComponent {
     // let routeFilters = new Map();
     // routeFilters.set('type', [0,1]);
     // this.setRouteFilters(routeFilters);
-    // this.getRoutes();
+    // this.getRoutes()
+    // .then((data) => {
+    //   console.log(data.values());
+    // });
 
     // FOR PROBLEM 2; Filter the request for each of the aforementioned subway routes
-    let stopFilters = new Map();
-    stopFilters.set('route', 'Orange');
-    this.setStopFilters(stopFilters);
-    this.getStops();
+    if (this.subwayRoutes.size == 0) {
+      let routeFilters = new Map();
+      routeFilters.set('type', [0,1]);
+      this.setRouteFilters(routeFilters);
+      return this.getRoutes()
+        .then((data) => {
+          return this.findStopsForEachRoute(data)
+        });
+    }
+    else {
+      return this.findStopsForEachRoute(this.subwayRoutes);
+    }
   }
 
   getRoutes() {
       return this.appService.getRoutes(this.routeFilters)
-      .then(function (data) {
-        let long_names = []
+      .then((data) => {
         for (let route of data) {
-          long_names.push(route.attributes.long_name); 
+          this.subwayRoutes.set(route.id, route.attributes.long_name);
         }
-        return long_names;
-      })
-      .then(function (data) {
-        //this.subwayRoutes = data;
-        console.log(data);
-      })
+        return this.subwayRoutes;
+      });
     }
 
-    getStops() {
-      return this.appService.getStops(this.stopFilters)
-      .then(function (data) {
-        let stop_names = []
-        for (let stop of data) {
-          stop_names.push(stop.attributes.name); 
-        }
-        return stop_names;
-      })
-      .then(function (data) {
-        console.log(data);
-      })
-    }
-    
-    setRouteFilters(routeFilters: Map<any, any>) {
-      this.routeFilters = routeFilters;
-    }
+  getStops() {
+    return this.appService.getStops(this.stopFilters)
+    .then((data) => {
+      let stop_ids = []
+      for (let stop of data) {
+        stop_ids.push(stop.id); 
+      }
+      return stop_ids;
+    });
+  }
 
-    setStopFilters(stopFilters: Map<any, any>) {
-      this.stopFilters = stopFilters;
+  private findStopsForEachRoute(subwayRoutes: Map<string, string>) {
+    let maxStops = Number.MIN_SAFE_INTEGER, 
+        minStops = Number.MAX_SAFE_INTEGER;
+
+    for (let subwayRoute of Array.from(subwayRoutes.keys())) {
+      let stopFilters = new Map();
+      stopFilters.set('route', subwayRoute);
+      this.setStopFilters(stopFilters);
+      this.getStops()
+      .then((data) => {
+        if (data.length > maxStops) {
+          maxStops = data.length;
+          this.maxStopRoute = {
+            route: subwayRoutes.get(subwayRoute), 
+            count: data.length
+          };
+        }
+        if (data.length < minStops) {
+          minStops = data.length;
+          this.minStopRoute = {
+            route: subwayRoutes.get(subwayRoute), 
+            count: data.length
+          };
+        }
+        this.routeToStopsDict.set(subwayRoute, data);
+        console.log(this.routeToStopsDict.values());
+      });
     }
+  }
+
+  private findRoutesForEachStop(subwayRoutes: Map<string, string>, routeToStopsDict: Map<string, string>) {
+    let stopToRoutesDict;
+    stopToRoutesDict = new Map<any, []>();
+    for (let route of Array.from(routeToStopsDict.keys())) {
+      for (let stop of routeToStopsDict.get(route)) {
+        if (stopToRoutesDict.get(stop)) {
+          let routeList = stopToRoutesDict.get(stop);
+          routeList.push(route);
+          stopToRoutesDict.set(stop, routeList);
+        } else {
+          let routeList = [];
+          routeList.push(route);
+          stopToRoutesDict.set(stop, routeList);
+        }
+      }
+    }
+    console.log(stopToRoutesDict);
+  }
+
+  private setRouteFilters(routeFilters: Map<any, any>) {
+    this.routeFilters = routeFilters;
+  }
+
+  private setStopFilters(stopFilters: Map<any, any>) {
+    this.stopFilters = stopFilters;
+  }
+
+  private async asyncForEach(array, callback) {
+    for (let index = 0; index < array.length; index++) {
+      await callback(array[index], index, array);
+    }
+  }
 }
